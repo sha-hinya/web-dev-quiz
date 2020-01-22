09ea96c8beea08f9b08298ced116b177b5b20f33
Shahinya Haeckel <sha.haeckel@gmail.com>
2020-01-22 13:27:42 +0100

    splited data to data.js

--- script.js
+++ script.js
@@ -1,74 +1,50 @@
-let questionArr = [
-  {
-    question: "HTML - what is true for a block level element?",
-    correctAnswer: "<section> is a block level element",
-    wrongAnswer1: "only take up as much space as necessary",
-    wrongAnswer2: "both answers above are true"
-  },
-  {
-    question: "HTML - Which of the following are empty tags, meaning no closing tag is needed?",
-    correctAnswer: "<br> tag",
-    wrongAnswer1: "<a> tag",
-    wrongAnswer2: "both tags are empty tags"
-  },
-  {
-    question: "CSS - Choose the correct selector specificity in a descending order?",
-    correctAnswer: "Inline > ID > Class > Type",
-    wrongAnswer1: "Inline > Class > ID > Type",
-    wrongAnswer2: "ID > Class > Inline > Type"
-  },
-  {
-    question: "CSS - given the following properties. Choose the only valid option to CSS …",
-    correctAnswer: "text-color: white;",
-    wrongAnswer1: "direction-flex: row;",
-    wrongAnswer2: "align-self: center"
-  },
-  {
-    question: "What are the names of our Bootcamp Web Dev dogs?",
-    correctAnswer: "Tom and Jerry",
-    wrongAnswer1: "Touve and Emma",
-    wrongAnswer2: "Alfa and Dalula"
-  },
-  {
-    question: "Event Loop",
-    correctAnswer: "O1",
-    wrongAnswer1: "O2",
-    wrongAnswer2: "O3"
-  },
-  {
-    question: "JS - For interpolation of a variable in a string, we use …",
-    correctAnswer: "backticks",
-    wrongAnswer1: "square bracket",
-    wrongAnswer2: "single quates "
-  },
-  {
-    question: "JS - What is true about the .includes () method?",
-    correctAnswer: "it is not case sensitive ",
-    wrongAnswer1: "returns a boolean ",
-    wrongAnswer2: "does not work on strings "
-  },
-  {
-    question: "JS - The return number of .localeCompare method is …",
-    correctAnswer: "-1 if reference string is after compared string ",
-    wrongAnswer1: "1 if reference string is before compared string",
-    wrongAnswer2: "depends on the length of the string"
-  },
-  {
-    question: "JS - Statements with logical operators",
-    correctAnswer: "!(4 > 2) 	// => true",
-    wrongAnswer1: "false && (anything) is a short-circuit evaluated to true",
-    wrongAnswer2: "true  || false      // => true"
-  },
-  {
-    question: "JS - what is the difference between .slice() and .substring()",
-    correctAnswer: "there is no difference ",
-    wrongAnswer1: ".slice() also supports a negative number",
-    wrongAnswer2: ".substring() also supports a negative number"
-  },
-  {
-    question: "JS - Boolean, undefined, symbol and null. Which of this primitives and immuntable?",
-    correctAnswer: "they all are primitives and immutable",
-    wrongAnswer1: "boolean and null",
-    wrongAnswer2: "undefined and null"
+function checkCurrentAnswer(event) {
+  let answer = event.currentTarget.innerHTML;
+
+  let correct = questionArr[currentQuestion - 1].correctAnswer;
+
+  //is user answer== to correct answer
+}
+
+function showQuestion() {
+  console.log(questionArr.length);
+  console.log(currentQuestion);
+  let currentQuestionIndex = currentQuestion;
+  //TODO make options appear at random position
+  questionBoxH3.innerHTML = questionArr[currentQuestionIndex].question;
+
+  let arr = [0, 1, 2];
+  //shuffle arr
+
+  options[arr[0]].innerHTML = questionArr[currentQuestionIndex].correctAnswer;
+  options[arr[1]].innerHTML = questionArr[currentQuestionIndex].wrongAnswer1;
+  options[arr[2]].innerHTML = questionArr[currentQuestionIndex].wrongAnswer2;
+
+  if (currentQuestionIndex < questionArr.length - 1) {
+    currentQuestion++;
   }
-];
+}
+
+function finalResult() {
+  //show final score
+}
+
+const body = document.getElementsByTagName("body");
+const mainContainer = document.getElementsByClassName(".mainContainer");
+const questionBox = document.getElementsByClassName(".questionBox");
+const questionBoxH3 = document.querySelector(".questionBox > h3");
+const answerBox = document.getElementsByClassName(".answerBox");
+const options = document.querySelectorAll(".options button");
+const btnNext = document.querySelector(".btn-next");
+let currentQuestion = 0;
+let score = 0;
+// let firstQuestion = questionArr[0].question;
+// let secondQuestion = questionArr[1].question;
+
+btnNext.onclick = showQuestion;
+
+options.forEach(function(button) {
+  button.onclick = checkCurrentAnswer;
+});
+
+showQuestion();
