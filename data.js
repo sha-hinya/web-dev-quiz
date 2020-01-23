63b5e1f1c0191a3058f98385f7e6e5c143edf632
Shahinya Haeckel <sha.haeckel@gmail.com>
2020-01-23 15:59:57 +0100

    clean data, embed html tag in js

--- data.js
+++ data.js
@@ -9,70 +9,70 @@ let questionArr = [
     question: "HTML - what is true for a block level element?",
     correctAnswer: "&lt;section&gt; is a block level element",
     wrongAnswer1: "only take up as much space as necessary",
-    wrongAnswer2: "both answers above are true"
+    wrongAnswer2: "both answers are true"
   },
   {
     question:
-      "HTML - Which of the following are empty tags, meaning no closing tag is needed?",
-    correctAnswer: "<br> tag",
-    wrongAnswer1: "<a> tag",
+      "HTML - Which of the following are empty tag(s), meaning no closing tag is needed?",
+    correctAnswer: "&lt;br&gt; tag",
+    wrongAnswer1: "&lt;a&gt; tag",
     wrongAnswer2: "both tags are empty tags"
   },
   {
     question:
       "CSS - Choose the correct selector specificity in a descending order?",
-    correctAnswer: "Inline > ID > Class > Type",
-    wrongAnswer1: "Inline > Class > ID > Type",
-    wrongAnswer2: "ID > Class > Inline > Type"
+    correctAnswer: "Inline &gt; ID &gt; Class &gt; Type",
+    wrongAnswer1: "Inline &gt; Class > ID &gt; Type",
+    wrongAnswer2: "ID &gt; Class &gt; Inline &gt; Type"
   },
   {
     question:
       "CSS - given the following properties. Choose the only valid option to CSS …",
-    correctAnswer: "text-color: white;",
+    correctAnswer: "align-self: center",
     wrongAnswer1: "direction-flex: row;",
-    wrongAnswer2: "align-self: center"
+    wrongAnswer2: "text-color: white;"
+  },
+  {
+    question: "JS - For an interpolation of a variable in a string, we use …",
+    correctAnswer: "backticks",
+    wrongAnswer1: "square bracket",
+    wrongAnswer2: "single quates "
+  },
+  {
+    question: "JS - What is true about the .includes() method?",
+    correctAnswer: "returns a boolean",
+    wrongAnswer1: "it is not case sensitive",
+    wrongAnswer2: "does not work on strings "
+  },
+  {
+    question: "JS - The return number of .localeCompare method is …",
+    correctAnswer: "-1 if reference string is before compared string ",
+    wrongAnswer1: "1 if reference string is before compared string",
+    wrongAnswer2: "depends on the length of the string"
+  },
+  {
+    question: "JS -Choose the correct statement of the logical operators", //prüfen
+    correctAnswer: "!(4 &gt; 2) 	// =&gt; false",
+    wrongAnswer1: "false && (anything) is a short-circuit evaluated to true",
+    wrongAnswer2: "true || false // =&gt; false"
+  },
+  {
+    question: "JS - what is the difference between .slice() and .substring()",
+    correctAnswer: ".slice() also supports a negative number",
+    wrongAnswer1: "there is no difference, they can be used equivalently",
+    wrongAnswer2: ".substring() also supports a negative number"
+  },
+  {
+    question:
+      "JS - Boolean, undefined, symbol and null. Which of this are primitives and immuntable?",
+    correctAnswer: "they all are primitives and immutable",
+    wrongAnswer1: "boolean and null",
+    wrongAnswer2: "undefined and null"
   },
   {
     question: "Event Loop",
     correctAnswer: "O1",
     wrongAnswer1: "O2",
     wrongAnswer2: "O3"
-  },
-  {
-    question: "JS - For interpolation of a variable in a string, we use …",
-    correctAnswer: "backticks",
-    wrongAnswer1: "square bracket",
-    wrongAnswer2: "single quates "
-  },
-  {
-    question: "JS - What is true about the .includes () method?",
-    correctAnswer: "it is not case sensitive ",
-    wrongAnswer1: "returns a boolean ",
-    wrongAnswer2: "does not work on strings "
-  },
-  {
-    question: "JS - The return number of .localeCompare method is …",
-    correctAnswer: "-1 if reference string is after compared string ",
-    wrongAnswer1: "1 if reference string is before compared string",
-    wrongAnswer2: "depends on the length of the string"
-  },
-  {
-    question: "JS - Statements with logical operators",
-    correctAnswer: "!(4 > 2) 	// => true",
-    wrongAnswer1: "false && (anything) is a short-circuit evaluated to true",
-    wrongAnswer2: "true  || false      // => true"
-  },
-  {
-    question: "JS - what is the difference between .slice() and .substring()",
-    correctAnswer: "there is no difference ",
-    wrongAnswer1: ".slice() also supports a negative number",
-    wrongAnswer2: ".substring() also supports a negative number"
-  },
-  {
-    question:
-      "JS - Boolean, undefined, symbol and null. Which of this primitives and immuntable?",
-    correctAnswer: "they all are primitives and immutable",
-    wrongAnswer1: "boolean and null",
-    wrongAnswer2: "undefined and null"
   }
 ];
